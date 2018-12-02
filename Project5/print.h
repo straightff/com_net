862e56bb0a6d099497ed935ce3f3c82a5e8ac808
qiu <2575378580@qq.com>
2018-12-02 19:57:42 +0800

    1

--- Project5/print.h
+++ Project5/print.h
@@ -1,12 +1,12 @@
 #pragma once
 #include"data.h"
-void sc()
+void sc()//这是一个注释
 {
 	for (int i = 1; i <= n; i++)
 	{
 		if (dis[i] < 200)
-			cout << m << "->" << i << ":" << "��һ����" << xia[m][i] << "���룺" << dis[i] << endl;
+			cout << m << "->" << i << ":" << "ÏÂÒ»Ìø£º" << xia[m][i] << "¾àÀë£º" << dis[i] << endl;
 		else
-			cout << m << "->" << i << ":�Ͽ�" << endl;
+			cout << m << "->" << i << ":¶Ï¿ª" << endl;
 	}
 }
