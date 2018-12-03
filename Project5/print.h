f160c97184006e9bf5d4b6b7bb8e1fcfa9e97c28
qiu <2575378580@qq.com>
2018-12-03 18:09:45 +0800

    a

--- Project5/print.h
+++ Project5/print.h
@@ -1,12 +1,12 @@
-﻿#pragma once
+#pragma once
 #include"data.h"
 void sc()//这是一个注释hhjjjjjjj
 {
 	for (int i = 1; i <= n; i++)
 	{
 		if (dis[i] < 200)
-			cout << m << "->" << i << ":" << "ÏÂÒ»Ìø£º" << xia[m][i] << "¾àÀë£º" << dis[i] << endl;
+			cout << m << "->" << i << ":" << "Next:" << xia[m][i] << "   Distance:" << dis[i] << endl;
 		else
-			cout << m << "->" << i << ":¶Ï¿ª" << endl;
+			cout << m << "->" << i << "Break" << endl;
 	}
 }
